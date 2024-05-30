ccc3b6a0e16cb6ee14a824bb17c87905e7c34e04
Mary Nguyen <marynguyen27@gmail.com>
2024-05-29 19:39:46 -0700

    classroom example

--- classes-context.js
+++ classes-context.js
@@ -32,3 +32,49 @@ dog.greet();
 
 let goldfish = new Pet('Nemo', 'Goldfish');
 goldfish.greet();
+
+// dog.name = 'Spike'
+// dog.greet ()
+// goldfish.greet ()
+
+class Student {
+  constructor(name, email) {
+    this.grades = [];
+    this.name = name;
+    this.email = email;
+    this.schedule = []; //maybe an array of date/time objects?
+  }
+
+  addLetterGrade(num) {
+    let letter;
+    if (num >= 90) {
+      letter = 'A';
+    } else if (num >= 80) {
+      letter = 'B';
+    } else if (num >= 70) {
+      letter = 'C';
+    } else if (num >= 60) {
+      letter = 'D';
+    } else {
+      letter = 'F';
+    }
+
+    this.grades.push(letter);
+  }
+}
+
+let classroom = [];
+let bob = new Student('bob', 'bob@gmail.com');
+let sarah = new Student('sarah', 'sarah@gmail.com');
+
+classroom.push(bob);
+classroom.push(sarah);
+
+bob.addLetterGrade(98);
+sarah.addLetterGrade(86);
+
+// making a student without using a variable
+classroom.push(new Student('Casey', 'casey@gmail.com'));
+classroom[2].addLetterGrade(72);
+
+console.log(classroom);
